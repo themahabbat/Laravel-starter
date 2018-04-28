4844892c08ac45c9502242eb56a0c5890f410ae4
Mahabbat Mammadov <mahabbatmusic@gmail.com>
2018-04-29 00:29:20 +0400

    Update web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -2,13 +2,14 @@
 
 
 Route::view('/', 'pages.index');
-Route::view('/signin', 'pages.auth.signin')->middleware('guest');
-Route::view('/signup', 'pages.auth.signup')->middleware('guest');
 
 // Auth
 Route::any('/signout', 'AuthCR@logout')->middleware('auth');
 
 Route::middleware('guest')->group(function(){
+  Route::view('/signin', 'pages.auth.signin');
+  Route::view('/signup', 'pages.auth.signup');
+  
   Route::post('/signin', 'AuthCR@login')->name('login');
   Route::post('/signup', 'AuthCR@register')->name('register');
 });
