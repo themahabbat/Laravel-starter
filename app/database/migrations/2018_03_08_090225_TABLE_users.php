e4767e98d36aafb2a97183e2470f36bca730106e
Mahabbat Mammadov <mahabbatmusic@gmail.com>
2018-04-28 23:52:14 +0400

    Update and rename 2018_03_08_090225_t_a_b_l_e_users.php to 2018_03_08_090225_TABLE_users.php

--- app/database/migrations/2018_03_08_090225_TABLE_users.php
+++ app/database/migrations/2018_03_08_090225_TABLE_users.php
@@ -17,6 +17,7 @@ class TABLEUsers extends Migration
           $users->increments('id');
           $users->string('email')->unique();
           $users->string('password');
+          $users->string('remember_token');
           $users->timestamps();
         });
     }
